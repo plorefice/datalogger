4cc994d20515009c49448b71b3a1c5e1705159bc
Pietro Lorefice <pietro.lorefice@gmail.com>
2020-05-13 21:45:18 +0200

    Implement heartbeat led

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,48 @@
 #![no_main]
 #![no_std]
 
-use cortex_m_semihosting::hprintln;
 use panic_semihosting as _;
+use rtfm::cyccnt::U32Ext;
+use stm32f4xx_hal::{
+    gpio::{gpiod::PD12, GpioExt, Output, PushPull},
+    hal::digital::v2::ToggleableOutputPin,
+};
 
-#[rtfm::app(device = stm32f4xx_hal::stm32, peripherals = true)]
+#[rtfm::app(device = stm32f4xx_hal::stm32, peripherals = true, monotonic = rtfm::cyccnt::CYCCNT)]
 const APP: () = {
-    #[init]
-    fn init(_: init::Context) {
-        hprintln!("Hello, World!").ok();
+    struct Resources {
+        heartbeat_led: PD12<Output<PushPull>>,
+    }
+
+    #[init(schedule = [heartbeat])]
+    fn init(mut cx: init::Context) -> init::LateResources {
+        // Initialize (enable) the monotonic timer (CYCCNT)
+        cx.core.DWT.enable_cycle_counter();
+
+        let gpiod = cx.device.GPIOD.split();
+
+        let heartbeat_led = gpiod.pd12.into_push_pull_output();
+
+        cx.schedule.heartbeat(cx.start).ok();
+
+        init::LateResources { heartbeat_led }
+    }
+
+    #[task(schedule = [heartbeat], resources = [heartbeat_led])]
+    fn heartbeat(cx: heartbeat::Context) {
+        static mut DELAY_PATTERN: [u32; 4] = [1, 3, 1, 20];
+        static mut I: usize = 0;
+
+        cx.schedule
+            .heartbeat(cx.scheduled + (DELAY_PATTERN[*I] * 1_000_000).cycles())
+            .ok();
+
+        cx.resources.heartbeat_led.toggle().ok();
+
+        *I = (*I + 1) % DELAY_PATTERN.len();
+    }
+
+    extern "C" {
+        fn UART4();
     }
 };
