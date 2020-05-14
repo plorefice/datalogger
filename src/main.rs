b2827391591343399f9e236f0142de7b382566af
Pietro Lorefice <pietro.lorefice@gmail.com>
2020-05-14 20:12:35 +0200

    Detect SD card presence

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,9 @@ use rtfm::cyccnt::U32Ext;
 use stm32f4xx_hal::{
     dwt::{Dwt, DwtExt},
     gpio::{gpioa::PA8, gpiod::PD12, GpioExt, OpenDrain, Output, PushPull, Speed::VeryHigh},
-    hal::digital::v2::ToggleableOutputPin,
+    hal::{blocking::delay::DelayMs, digital::v2::ToggleableOutputPin},
     rcc::RccExt,
+    sdio::{ClockFreq, Sdio},
 };
 
 #[rtfm::app(device = stm32f4xx_hal::stm32, peripherals = true, monotonic = rtfm::cyccnt::CYCCNT)]
@@ -18,6 +19,7 @@ const APP: () = {
     struct Resources {
         dwt: Dwt,
         dht11: Dht11<PA8<Output<OpenDrain>>>,
+        sdcard: SdCard,
         heartbeat_led: PD12<Output<PushPull>>,
     }
 
@@ -33,10 +35,32 @@ const APP: () = {
         let dwt = cx.core.DWT.constrain(cx.core.DCB, clocks);
 
         let gpioa = cx.device.GPIOA.split();
+        let gpioc = cx.device.GPIOC.split();
         let gpiod = cx.device.GPIOD.split();
 
+        // Create DHT11 instance on PA8
         let dht11 = Dht11::new(gpioa.pa8.into_open_drain_output().set_speed(VeryHigh));
+
+        // Create heartbeat led instance
         let heartbeat_led = gpiod.pd12.into_push_pull_output();
+
+        // Create sd-card instance
+        let mut sdio = Sdio::new(
+            cx.device.SDIO,
+            (
+                gpioc.pc12.into_alternate_af12().internal_pull_up(false),
+                gpiod.pd2.into_alternate_af12().internal_pull_up(true),
+                gpioc.pc8.into_alternate_af12().internal_pull_up(true),
+                gpioc.pc9.into_alternate_af12().internal_pull_up(true),
+                gpioc.pc10.into_alternate_af12().internal_pull_up(true),
+                gpioc.pc11.into_alternate_af12().internal_pull_up(true),
+            ),
+        );
+
+        // Wait for an SD card to be inserted
+        while sdio.init_card(ClockFreq::F400Khz).is_err() {
+            dwt.delay().delay_ms(1_000_u32);
+        }
 
         cx.schedule.sensor_reading(cx.start).ok();
         cx.schedule.heartbeat(cx.start).ok();
@@ -44,23 +68,29 @@ const APP: () = {
         init::LateResources {
             dwt,
             dht11,
+            sdcard: SdCard(sdio),
             heartbeat_led,
         }
     }
 
-    #[task(schedule = [sensor_reading], resources = [dwt, dht11])]
+    #[task(schedule = [sensor_reading], spawn = [save_data], resources = [dwt, dht11])]
     fn sensor_reading(cx: sensor_reading::Context) {
         static READING_PERIOD: u32 = 500_000_000;
 
         let mut delay = cx.resources.dwt.delay();
 
         if let Ok(meas) = cx.resources.dht11.perform_measurement(&mut delay) {
-            hprintln!("{:?}", meas).ok();
+            cx.spawn.save_data(meas).unwrap();
         }
 
         cx.schedule
             .sensor_reading(cx.scheduled + READING_PERIOD.cycles())
             .ok();
+    }
+
+    #[task(resources = [sdcard])]
+    fn save_data(_: save_data::Context, meas: dht11::Measurement) {
+        hprintln!("{:?}", meas).ok();
     }
 
     #[task(schedule = [heartbeat], resources = [heartbeat_led])]
@@ -81,3 +111,5 @@ const APP: () = {
         fn UART4();
     }
 };
+
+pub struct SdCard(Sdio);
